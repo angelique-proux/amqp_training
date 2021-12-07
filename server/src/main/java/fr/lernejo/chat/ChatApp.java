9a0ffce268a10ccf30b529222d640efc6c9b2530
angelique2000 <angelique.proux@gmail.com>
2021-12-07 14:52:23 +0100

    AMQP Client

--- server/src/main/java/fr/lernejo/chat/ChatApp.java
+++ server/src/main/java/fr/lernejo/chat/ChatApp.java
@@ -2,6 +2,7 @@ package fr.lernejo.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class ChatApp {
